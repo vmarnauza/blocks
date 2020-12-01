f42d9b0f099bbb8d8bf9db28b3dc08bea164bb3d
Viesturs Marnauza <v.marnauza@gmail.com>
2020-12-01 21:12:47 +0200

    Add more colors

--- src/colors.js
+++ src/colors.js
@@ -1,9 +1,14 @@
 export const colors = {
-  pink: "#BF5E84",
+  darkblue: "#493271",
   purple: "#71427E",
-  vanilla: "#A4AFC5",
-  lightblue: "#DAD6DA",
+  pink: "#B04D66",
+  vanilla: "#CFC7CF",
+  lightblue: "#989EB6",
+  orange: "#D57467",
+  yellow: "#E6D4BC",
 };
+
+export const groundColor = "#41395D";
 
 export const random = () => {
   const colorArray = Object.values(colors);
